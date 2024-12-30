d11eaa8dc567b70c63cf24c611816e062789d9df
Kacaii <kacaii.privado@gmail.com>
2024-12-30 00:05:27 -0300

    enable goroutines

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 const GREEN string = "\x1b[32m"
@@ -39,51 +40,54 @@ func main() {
 		{"04 ARQUIVE", arquiveDesc},
 	}
 
-	fmt.Printf("Generating PARA structure in: %s \n", baseDir) // Feedback message for the user.
-
-	err = generateParaDirectories(paraStructure, baseDir)
-	if err != nil {
-		log.Fatal("Failed to generate directories", err)
+	if baseDir == "." {
+		fmt.Println("Generating PARA structure in the current directory 󰣞")
+	} else {
+		fmt.Println("Generating PARA structure in:", baseDir, "󰣞")
 	}
 
-	err = writeFileContent(paraStructure, baseDir)
-	if err != nil {
-		log.Fatal("Failed to write content to README files", err)
+	var wg sync.WaitGroup
+	for _, dir := range paraStructure {
+		// Add one (1) to the waitGroups for every directory in the structure
+		wg.Add(1)
+
+		// Spawn goroutines  to generate the whole file structure
+		go func(d paraDirectory) {
+			defer wg.Done()
+
+			err = generateParaDirectory(d, baseDir)
+			if err != nil {
+				log.Println("Failed to create directory:", d.name, err.Error())
+			}
+
+			err = writeReadme(d, baseDir)
+			if err != nil {
+				log.Println("Failed to write README for:", d.name, err.Error())
+			}
+		}(dir) // Passing every directory from the structure as an arguemnt to the function
 	}
+
+	wg.Wait() // Waiting for every file to be written
 
 	fmt.Println(GREEN + "PARA Structure Generated Successfully Using Golang! 󱜙  ") // All done! 
 }
 
 // Writes content to the PARA Files
-func writeFileContent(paraStructure []paraDirectory, baseDirectory string) error {
-	for _, dir := range paraStructure {
-		// Generating a path for every README file
-		filePath := filepath.Join(baseDirectory, dir.name, "README.md")
-
-		// Writing contents to the README files
-		err := os.WriteFile(filePath, []byte(dir.readMeContent), os.ModePerm)
-		if err != nil {
-			return err
-		}
+func writeReadme(dir paraDirectory, baseDirectory string) error {
+	filePath := filepath.Join(baseDirectory, dir.name, "README.md")
+	// Writing contents to the README files
+	err := os.WriteFile(filePath, []byte(dir.readMeContent), os.ModePerm)
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
 // Generates the necessary Directories for the structure: PROJECTS, AREAS, RESOURCES and ARQUIVE
-func generateParaDirectories(structure []paraDirectory, baseDir string) error {
-	for _, dir := range structure {
-		// Creates a path for every directory of the structure
-		pathToDirectory := filepath.Join(baseDir, dir.name)
-
-		// Generates a directory for every part of the structure
-		err := os.MkdirAll(pathToDirectory, os.ModePerm)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+func generateParaDirectory(dir paraDirectory, baseDir string) error {
+	pathToDirectory := filepath.Join(baseDir, dir.name)
+	return os.MkdirAll(pathToDirectory, os.ModePerm)
 }
 
 // Returns an error if the path provided dont exist or if is not a directory.
